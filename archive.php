5b54ebc8fe8ace1cd305f6aa69a26f2eaff383b0
Norman <norman.primary@gmail.com>
2022-09-20 14:25:23 +0700

    Implement Archive

---
+++ archive.php
@@ -0,0 +1,31 @@
+<!-- Template for displaying archive pages -->
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+    <div id="main" class="site-main">
+        <?php if(have_posts( )): ?>
+            <header class="archive-page-header">
+                <?php 
+                    the_archive_title('<h1 class="archive-title">','</h1>' );
+                    the_archive_description('<div class="archive-description">','</div>' );
+                ?>
+            </header>
+            <?php 
+                while(have_posts( )):
+                    the_post();
+                    get_template_part('template-parts/post/content' );
+                endwhile;
+
+                echo paginate_links(
+                    [
+                        'prev_text' => esc_html__('Sebelumnya', 'firstwp'),
+                        'next_text' => esc_html__('Selanjutnya', 'firstwp')
+                    ]
+                );
+            else : 
+                get_template_part('template-parts/page/content', 'none');
+            ?>
+
+        <?php endif ?>
+    </div>
+</div>
+<?php get_footer(); ?>
